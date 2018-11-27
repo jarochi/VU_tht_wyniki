4e2570dc85247c02b2fb9f47496f1f4b3c6caa61
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-11-27 09:26:05 +0100

    $ -> [[]] fix

--- heatmap.R
+++ heatmap.R
@@ -7,18 +7,19 @@ library(tidyverse)
 
 peptydy <- read.csv("amyloid_heatmap.csv")
 peptydy <- peptydy[1:(nrow(peptydy)-1),]
-peptydy <- peptydy %>% mutate(match = ifelse(peptydy$database == peptydy$tht, "yes", "no"))
+peptydy <- peptydy %>% mutate(match = ifelse(peptydy[["AmyLoad"]] == peptydy[["ThT"]], "yes", "no"))
 
 peptydy <- peptydy %>% gather("method", "result", 2:4)
 
+pep_order <- filter(peptydy, method == "AmyLoad") %>% arrange(result) %>%  pull(peptide) %>% as.character
 
-
+peptydy[["peptide"]] <- factor(peptydy[["peptide"]], levels = pep_order)
 
 ggplot(peptydy, aes(x = method, y = peptide, fill = result)) +
   geom_tile() +
   geom_tile(aes(fill = result), color = "white") +
   # scale_fill_manual(values=c("red", "lightgreen")) +
-  scale_x_discrete(limits=c("database","tht","match")) +
+  scale_x_discrete(limits=c("AmyLoad","ThT","match")) +
   theme_bw()
 
 
